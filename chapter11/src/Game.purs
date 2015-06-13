d9a9da7737b178159b23dfef55944ebf7e496359
Becky Conning <becky.conning@icloud.com>
2015-06-13 16:50:16 +0100

    Started exercises 11.12

--- chapter11/src/Game.purs
+++ chapter11/src/Game.purs
@@ -30,7 +30,7 @@ pickUp :: GameItem -> Game Unit
 pickUp item = do
   GameState state <- get
   case state.player `M.lookup` state.items of
-    Just items 
+    Just items
       | item `S.member` items -> do
           let newItems = M.update (Just <<< S.delete item) state.player state.items
               newInventory = S.insert item state.inventory
@@ -71,32 +71,36 @@ game ["look"] = do
   describeRoom
   for_ (M.lookup state.player state.items) $ \items ->
     tell ((\item -> "You can see the " ++ show item ++ ".") <$> S.toList items)
+
 game ["inventory"] = do
   GameState state <- get
   tell ((\item -> "You have the " ++ show item ++ ".") <$> S.toList state.inventory)
+
 game ["north"] = move 0    1
 game ["south"] = move 0    (-1)
 game ["west"]  = move (-1) 0
 game ["east"]  = move 1    0
-game ["take", item] =
-  case readItem item of
-    Nothing -> tell ["I don't know what item you are referring to."]
-    Just gameItem -> pickUp gameItem
-game ["use", item] =
-  case readItem item of
-    Nothing -> tell ["I don't know what item you are referring to."]
-    Just gameItem -> do
-      hasItem <- has gameItem
-      if hasItem
-        then use gameItem
-        else tell ["You don't have that item."]
+
+game ["take", item] = case readItem item of
+  Nothing -> tell ["I don't know what item you are referring to."]
+  Just gameItem -> pickUp gameItem
+
+game ["use", item] = case readItem item of
+  Nothing -> tell ["I don't know what item you are referring to."]
+  Just gameItem -> do
+    hasItem <- has gameItem
+    if hasItem
+      then use gameItem
+      else tell ["You don't have that item."]
+
 game ["debug"] = do
   GameEnvironment env <- ask
   if env.debugMode
     then do
       state <- get
       tell [show state]
-    else tell ["Not running in debug mode."]  
-game [] = return unit
-game _  = tell ["I don't understand."]
+    else tell ["Not running in debug mode."]
 
+game [] = return unit
+
+game _  = tell ["I don't understand."]
